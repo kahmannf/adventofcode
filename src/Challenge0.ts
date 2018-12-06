b309344a46501667e5d3c4198faca61d6201d0ad
Felix Kahmann <f.kahmann@reisewitz.com>
2018-12-06 14:58:11 +0100

    add a challenge-template

---
+++ src/Challenge0.ts
@@ -0,0 +1,23 @@
+import { Challenge } from './challenge';
+export class Challenge0 implements Challenge {
+  day = 0;
+
+  solve(input: string): string {
+    try {
+      throw new Error("Method not implemented.");
+    } catch(error) {
+      //console.log(error);
+      throw error;
+    }
+  }
+
+  solve2(input: string): string {
+    try {
+      throw new Error("Method not implemented.");
+    } catch(error) {
+      //console.log(error);
+      throw error;
+    }
+  }
+
+}
